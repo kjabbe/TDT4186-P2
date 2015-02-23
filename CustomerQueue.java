64186c277469e8ef3d8b354c5b94016eec824773
Tormod Bjørnhaug <tormod@sinepost.no>
2015-02-23 16:28:06 +0100

    First try to create thread in Doorman and to start an outline of the CustomerQueue

--- CustomerQueue.java
+++ CustomerQueue.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * This class implements a queue of customers as a circular buffer.
  */
@@ -7,17 +10,29 @@ public class CustomerQueue {
 	 * @param queueLength	The maximum length of the queue.
 	 * @param gui			A reference to the GUI interface.
 	 */
-
 	private int queueLength;
-
+	private Gui gui;
+	private Queue<Customer> queue;
+	
     public CustomerQueue(int queueLength, Gui gui) {
-		// Incomplete
 		this.queueLength = queueLength;
+		this.gui = gui;
+		this.queue = new LinkedList<Customer>();
+		
+    	// Incomplete
 	}
+    
+    public void NewCustomer(Customer c) {
+    	this.queue.add(c);
+    }
+    public Customer NextCustomer() {
+    	return this.queue.remove();
+    }
 
-	public int getQueueLength(){
-		return queueLength;
-	}
-
+    // AddCustomer
+    // queue length
+    // PopCustomer
+    // 
+    
 	// Add more methods as needed
 }
--- Doorman.java
+++ Doorman.java
@@ -1,33 +1,59 @@
+import java.util.Timer;
+
 /**
  * This class implements the doorman's part of the
  * Barbershop thread synchronization example.
  */
-public class Doorman {
+public class Doorman implements Runnable,Constants {
 	/**
 	 * Creates a new doorman.
 	 * @param queue		The customer queue.
 	 * @param gui		A reference to the GUI interface.
 	 */
-	public Doorman(CustomerQueue queue, Gui gui) { 
-		// Incomplete
-		
+	
+	private CustomerQueue queue;
+	private Gui gui;
+	private boolean active;
+	
+	public Doorman(CustomerQueue queue, Gui gui) {
+		this.queue = queue;
+		this.gui = gui;
+		this.active = true;
 	}
 
 	/**
 	 * Starts the doorman running as a separate thread.
 	 */
-	startThread(queue);
-	public void startThread(CustomerQueue queue) {
-		// Incomplete
-		int r = min+(int)(Math.random()*(max-min+1));
+	public void startThread() {
+		new Thread(new Doorman(queue,gui)).start();
 	}
 
 	/**
 	 * Stops the doorman thread.
 	 */
 	public void stopThread() {
-		// Incomplete
+		this.active = false;
 	}
+
+	public void run() {
+		int r;
+		int min = MIN_DOORMAN_SLEEP;
+		int max = MAX_DOORMAN_SLEEP;
+		while(active) {
+			r = min+(int)(Math.random()*(max-min+1));
+			try {
+				Thread.sleep(r);
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+
+
 
 	// Add more methods as needed
 }
