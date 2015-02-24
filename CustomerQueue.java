37c0904ba98f0d2369ab8c6b1eaeeda435271d01
inexpensive <medlem@inexpensive.ed.ntnu.no>
2015-02-24 11:33:42 +0100

    Updated Customer Queue: - Fix: fill lounge chair

--- CustomerQueue.java
+++ CustomerQueue.java
@@ -4,7 +4,7 @@ import java.util.Queue;
 /**
  * This class implements a queue of customers as a circular buffer.
  */
-public class CustomerQueue {
+public class CustomerQueue implements Constants {
 	/**
 	 * Creates a new customer queue.
 	 * @param queueLength	The maximum length of the queue.
@@ -13,17 +13,24 @@ public class CustomerQueue {
 	private int queueLength;
 	private Gui gui;
 	private Queue<Customer> queue;
+    private int pos;
 	
     public CustomerQueue(int queueLength, Gui gui) {
 		this.queueLength = queueLength;
 		this.gui = gui;
 		this.queue = new LinkedList<Customer>();
+        this.pos = NOF_CHAIRS;
 		
     	// Incomplete
 	}
     
+    private int position() {
+        return (getCustomerID() % this.pos)+1;
+    }
+
     public synchronized void NewCustomer(Customer c) {
     	this.queue.add(c);
+        BarbershopGui.fillLoungeChair(position(c), c)
     	this.notifyAll();
     }
 
